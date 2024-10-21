a9ba493ff8eda64a196f306c29ddce68ec26a272
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-10-21 16:37:41 -0300

    Create semaforo.cpp

---
+++ Av1/semaforo.cpp
@@ -0,0 +1,26 @@
+int led1 = 6;
+int led2 = 5;
+int led3 = 3;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led3, LOW);
+  delay(1000);
+}

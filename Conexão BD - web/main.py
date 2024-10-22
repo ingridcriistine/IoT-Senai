0f3b189a15a85700d4bf799d85c67d86a338b81d
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-10-22 16:20:36 -0300

    Create main.py

---
+++ Conexão BD - web/main.py
@@ -0,0 +1,28 @@
+import serial
+import time
+import mysql.connector
+
+ser = serial.Serial('COM3', 9600, timeout=1) # Iniciando a conexão serial
+con = mysql.connector.connect(host='paparella.com.br', database='paparell_iot', user='paparell_iot', password='senai2024') # Conectando no banco
+
+# Conferindo se a conexão com o banco foi bem sucedida
+if con.is_connected():
+    db_info = con.get_server_info()
+    print("Conectado, versão: ", db_info)
+    cursor = con.cursor()
+    cursor.execute("select database();")
+    linha = cursor.fetchone()
+    print("Conectado ao banco de dados ", linha)
+
+
+time.sleep(10) # Tempinho pq ninguém é de ferro
+
+# pegar do banco
+cursor.execute("select valor_sensor from bosch where nome_aluno = 'Ingrid-Milena'")
+data = cursor.fetchall()
+
+item = (data[len(data)-1]).strip("\rn")
+print(item)
+
+con.commit()
+ser.close()

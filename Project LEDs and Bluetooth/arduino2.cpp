f64a539981905c5309dd3b44c05ee304594bf8c8
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-10-25 20:08:06 -0300

    Create arduino2.cpp

---
+++ Project LEDs and Bluetooth/arduino2.cpp
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+//int valor = 0;
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+int led5 = 12;
+
+byte valor;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  pinMode(led5,OUTPUT);///
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()>0) {
+    valor = Serial.parseInt();  // Lê até receber uma nova linha   
+  }
+   if(valor==0){
+      digitalWrite(led1,LOW);
+      digitalWrite(led2,LOW);
+      digitalWrite(led3,LOW);
+      digitalWrite(led4,LOW);
+      digitalWrite(led5,LOW);
+
+      // digitalWrite(7, LOW);
+      digitalWrite(7, HIGH);
+
+    }else if (valor==1)
+    {
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,LOW);
+      digitalWrite(led3,LOW);
+      digitalWrite(led4,LOW);
+      digitalWrite(led5,LOW);
+      digitalWrite(7, HIGH);
+
+
+    }else if (valor==2)
+    {
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,HIGH);
+      digitalWrite(led3,LOW);
+      digitalWrite(led4,LOW);
+      digitalWrite(led5,LOW);
+
+      // digitalWrite(7, LOW);
+      digitalWrite(7, HIGH);
+
+
+    }else if (valor==3)
+    {
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,HIGH);
+      digitalWrite(led3,HIGH);
+      digitalWrite(led4,LOW);
+      digitalWrite(led5,LOW);
+
+      // digitalWrite(7, LOW);
+      digitalWrite(7, HIGH);
+
+
+    }else if (valor==4)
+    {
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,HIGH);
+      digitalWrite(led3,HIGH);
+      digitalWrite(led4,HIGH);
+      digitalWrite(led5,LOW);
+
+      digitalWrite(7, HIGH);
+
+    }else
+    {
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,HIGH);
+      digitalWrite(led3,HIGH);
+      digitalWrite(led4,HIGH);
+      digitalWrite(led5,HIGH);
+
+      digitalWrite(7, LOW);
+    }
+}
